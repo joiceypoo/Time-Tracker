e76c678b4dd1e02b24089435588764f707b1f875
Onyekachi Ezeoke <onyekachiezeoke@Onyekachis-MacBook-Pro.local>
2018-12-11 21:56:49 +0100

    setup views

--- Time-Tracker/Source/App/Other/HomeController.swift
+++ Time-Tracker/Source/App/Other/HomeController.swift
@@ -10,10 +10,43 @@ import UIKit
 
 class HomeController: UIViewController {
 
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var eventsTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Time Tracker"
+        eventsTable.delegate = self
+        eventsTable.dataSource = self
+        setupView()
+        
     }
 
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+    }
+    
+    private func setupView() {
+        addButton.layer.cornerRadius = 20
+        addButton.layer.masksToBounds = true
+        addButton.tintColor = .blue
+        let image = UIImage(named: "plus")?.withRenderingMode(.alwaysTemplate)
+        addButton.setImage(image, for: .normal)
+    }
+    
+}
 
+extension HomeController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell", for: indexPath)
+//        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
 }
 
